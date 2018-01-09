35bdca4f5cacac158b676107ecdf68bc6a08e4e0
vagrant <vagrant@vagrant.vm>
2018-01-09 00:43:34 +0000

    Compled words_count function

---
+++ wordcount.py
@@ -0,0 +1,21 @@
+def count_words(file):
+
+    open_file = open('./twain.txt')
+
+    words_count = {}
+
+    for line in open_file:
+        line = line.rstrip()
+        words = line.split()
+        
+        for word in words:
+            words_count[word] = words_count.get(word, 0) + 1
+
+    for word, count in words_count.iteritems():
+        print word, count
+    
+
+    open_file.close()
+
+#count_words('./test.txt')
+count_words('./twain.txt')
